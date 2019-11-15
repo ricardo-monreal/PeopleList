9970766c697c939cf271b2d71c5973d7768d9f7e
Ricardo <moises.monreal@gmail.com>
2019-11-15 01:00:48 -0700

    working list

--- app/src/main/java/com/example/peoplelist/MainActivity.java
+++ app/src/main/java/com/example/peoplelist/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.peoplelist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_add, btn_sortABC, btn_sortAGE;
+
+    ListView lv_friendsList;
+
+    PersonAdapter adapter;
+
+    MyFriends myFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_ADD);
+        btn_sortABC = findViewById(R.id.btn_sortABC);
+        btn_sortAGE = findViewById(R.id.btn_sortAGE);
+        lv_friendsList = findViewById(R.id.lv_listofnames);
+
+        myFriends = new MyFriends();
+
+        adapter = new PersonAdapter(MainActivity.this, myFriends);
+
+        lv_friendsList.setAdapter(adapter);
+
     }
 }
